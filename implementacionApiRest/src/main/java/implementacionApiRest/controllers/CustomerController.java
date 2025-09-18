787b9185921da957cb1af22835c77004d4a75d2e
GustavoRT <tanganica1@hotmail.com>
2025-09-18 10:38:49 +0200

    Iniciando microservicio Clase customer creada con enrutamiento endpoint

--- implementacionApiRest/src/main/java/implementacionApiRest/controllers/CustomerController.java
+++ implementacionApiRest/src/main/java/implementacionApiRest/controllers/CustomerController.java
@@ -2,9 +2,7 @@ package implementacionApiRest.controllers;
 
 import implementacionApiRest.domain.Customer;
 import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,13 +18,13 @@ public class CustomerController {
             new Customer(4,"Marcos Bermejo", "marcosb","123")
     ));
 
-    @GetMapping("clientes")
-    public List<Customer> getCustomers(){
+    @GetMapping("/clientes")
+    public List<Customer> getClientes(){
         return customers;
     }
 
-    @GetMapping("clientes/{username}")
-    public Customer getCustomer(@PathVariable String username){
+    @GetMapping("/clientes/{username}")
+    public Customer getCliente(@PathVariable String username){
         for (Customer customer : customers) {
             if (customer.getUsername().equalsIgnoreCase(username)) {
                 return customer;
@@ -34,4 +32,54 @@ public class CustomerController {
         }
         return null;
     }
+
+    @PostMapping("/clientes")
+    public Customer postCliente(@RequestBody Customer customer){
+        customers.add(customer);
+        return customer;
+    }
+
+    @PutMapping("clientes")
+    public Customer putClientes(@RequestBody Customer customer){
+        for(Customer customer1: customers){
+            if(customer.getID() == customer1.getID()){
+                customer1.setName(customer.getName());
+                customer1.setUsername(customer.getUsername());
+                customer1.setPassword(customer.getPassword());
+
+                return customer1;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/clientes/{ID}")
+    public Customer deleteClientes(@PathVariable int ID){
+        for(Customer customer: customers){
+            if(customer.getID() == ID){
+                customers.remove(customer);
+
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    @PatchMapping("/clientes")
+    public Customer patchCliente(@RequestBody Customer customer){
+        for(Customer c: customers){
+            if(c.getID() == customer.getID()){
+                if(customer.getName() != null){
+                    c.setName(customer.getName());
+                }if(customer.getUsername() != null){
+                    c.setUsername(customer.getUsername());
+                }if(customer.getPassword() != null){
+                    c.setPassword(customer.getPassword());
+                }
+                return c;
+            }
+        }
+        return null;
+    }
 }
+
